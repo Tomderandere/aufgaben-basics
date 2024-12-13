48fb8154edc42fdcccd2f8384ec74fd1851bb9bc
Tomderandere <tompbickle@gmail.com>
2024-12-13 14:46:14 +0100

    Warum geht des?

--- calculations/lists/min_list_recursive.go
+++ calculations/lists/min_list_recursive.go
@@ -4,8 +4,21 @@ package lists
 // Wenn die Liste leer ist, wird 0 zurückgegeben.
 // ZUSATZBEDINGUNG: Diese Funktion darf keine Schleife verwenden.
 func MinListRecursive(nums []int) int {
-	// TODO
-	return 0
+
+	// Basisfall: Wenn die Liste leer ist, gebe 0 zurück
+	if len(nums) == 0 {
+		return 0
+	}
+
+	// Rekursiver Fall: Das Minimum des ersten Elements und des Minimums des restlichen Teils der Liste
+	first := nums[0]
+	rest := MinListRecursive(nums[1:])
+
+	// Vergleiche das erste Element mit dem Minimum des Restes
+	if first < rest || rest == 0 {
+		return first
+	}
+	return rest
 }
 
 // REMARKS

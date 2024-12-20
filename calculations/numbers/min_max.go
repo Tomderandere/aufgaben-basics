e722110f7f09afaebb248e1263af2ccd66aeddf0
Tomderandere <tompbickle@gmail.com>
2024-12-20 15:23:14 +0100

    min max

--- calculations/numbers/min_max.go
+++ calculations/numbers/min_max.go
@@ -2,12 +2,16 @@ package numbers
 
 // Berechnet das Minimum von zwei Zahlen.
 func Min(a, b int) int {
-	// TODO
-	return 0
+	if a > b {
+		return b
+	}
+	return a
 }
 
 // Berechnet das Maximum von zwei Zahlen.
 func Max(a, b int) int {
-	// TODO
-	return 0
+	if a > b {
+		return b
+	}
+	return a
 }

6135ed77010918d1f5219c63ce36a696fadf183e
Tomderandere <tompbickle@gmail.com>
2024-12-20 15:12:15 +0100

    count divisor

--- calculations/numbers/count_divisors.go
+++ calculations/numbers/count_divisors.go
@@ -2,6 +2,12 @@ package numbers
 
 // Erwartet eine Zahl n >= 1 und liefert die Anzahl der Teiler dieser Zahl zurück.
 func CountDivisors(n int) int {
-	// TODO
-	return 0
+	zähler := 0
+	for i := 1; i <= n; i++ {
+		t := n % i
+		if t == 0 {
+			zähler++
+		}
+	}
+	return zähler
 }

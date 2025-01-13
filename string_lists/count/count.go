7bc693e796e65760ed8626c144965b999ac76dd8
Tomderandere <tompbickle@gmail.com>
2025-01-13 12:23:30 +0100

    count

--- string_lists/count/count.go
+++ string_lists/count/count.go
@@ -3,6 +3,11 @@ package count
 // Erwartet eine Liste von Strings sowie einen String, der gezählt werden soll.
 // Liefer die Anzahl der Vorkommen des gesuchten Strings in der Liste.
 func Count(strings []string, search string) int {
-	// TODO
-	return 0
+	var counter int
+	for i := 0; i < len(strings); i++ {
+		if strings[i] == search {
+			counter++
+		}
+	}
+	return counter
 }

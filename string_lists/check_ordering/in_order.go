adba4c687bae92a6ccc0932ebb1ec70508e44872
Tomderandere <tompbickle@gmail.com>
2025-01-14 09:22:10 +0100

    In Order

--- string_lists/check_ordering/in_order.go
+++ string_lists/check_ordering/in_order.go
@@ -4,11 +4,20 @@ package check_ordering
 // Überprüft, ob die beiden Strings in der Liste in der gegebenen Reihenfolge vorkommen.
 // Gibt `true` zurück, wenn das der Fall ist, ansonsten `false`.
 func CheckOrdering(strings []string, first, second string) bool {
-
-	if strings[1] == first {
-		//if strings[len(strings)-1] == second {
-		//	return true
-		//}
+	var ifirst int = -10
+	var isecond int = -10
+	if len(strings) != 0 {
+		for i := 0; i < (len(strings)); i++ {
+			if strings[i] == first {
+				ifirst = i
+			}
+			if strings[i] == second {
+				isecond = i
+			}
+		}
+	}
+	if ifirst != -10 && isecond != -10 && ifirst < isecond {
+		return true
 	}
 	return false
 }
--- string_lists/check_ordering/in_order_test.go
+++ string_lists/check_ordering/in_order_test.go
@@ -5,12 +5,16 @@ import "fmt"
 func ExampleCheckOrdering() {
 	fmt.Println(CheckOrdering([]string{"a", "b", "c", "d", "e", "f"}, "a", "f"))
 	fmt.Println(CheckOrdering([]string{"a", "b", "c", "d", "e", "f"}, "f", "a"))
+	fmt.Println(CheckOrdering([]string{"a", "b", "c", "e", "d", "f"}, "a", "f"))
+	fmt.Println(CheckOrdering([]string{"b", "a", "c", "e", "f", "d"}, "a", "f"))
 	fmt.Println(CheckOrdering([]string{"a", "b", "c", "d", "e", "f"}, "f", "g"))
 	fmt.Println(CheckOrdering([]string{}, "a", "b"))
 
 	// Output:
 	// true
 	// false
+	// true
+	// true
 	// false
 	// false
 }

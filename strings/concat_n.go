e9338188dc64981b646037f63832805701c629fe
Tomderandere <tompbickle@gmail.com>
2025-01-17 15:08:28 +0100

    ConcatN

--- strings/concat_n.go
+++ strings/concat_n.go
@@ -3,6 +3,13 @@ package strings
 // Erwartet zwei Strings s und sep sowie eine Zahl n.
 // Liefert einen String, der aus n Kopien von s besteht, die duch sep getrennt werden.
 func ConcatN(s, sep string, n int) string {
-	// TODO
-	return ""
+	var result string
+	for i := n; i > 0; i-- {
+		result = result + s
+		for i > 1 {
+			result = result + sep
+			break
+		}
+	}
+	return result
 }

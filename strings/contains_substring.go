f31ba173b77512f283bbf08e2e3b0defeed926cc
Tomderandere <tompbickle@gmail.com>
2025-01-31 14:48:30 +0100

    contains sub string

--- strings/contains_substring.go
+++ strings/contains_substring.go
@@ -2,6 +2,18 @@ package strings
 
 // Erwartet zwei Strings s und t und prüft, ob t in s als Teilstring vorkommt.
 func ContainsSubstring(s, t string) bool {
-	// TODO
+	for i := 0; i <= len(s)-len(t); i++ {
+		if s[i] == t[0] {
+			for n := 0; n < len(t); n++ {
+				if s[n+i] != t[n] {
+					break
+				}
+				if n == len(t)-1 {
+					return true
+				}
+			}
+		}
+
+	}
 	return false
 }

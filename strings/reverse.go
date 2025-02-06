cbfd12b87c51b8edeb63e5857999b1233768cdba
Tomderandere <tompbickle@gmail.com>
2025-02-06 17:41:53 +0100

    reverse

--- strings/reverse.go
+++ strings/reverse.go
@@ -2,14 +2,16 @@ package strings
 
 // Erwartet einen String s und liefert s rückwärts zurück.
 func Reverse(s string) string {
-	// TODO
-	return ""
+	var reverse string
+	for i := (len(s) - 1); i >= 0; i-- {
+		reverse = string(reverse) + string(s[i])
+	}
+	return reverse
 }
 
 // Erwartet zwei Strings s1 und s2 und prüft, ob der eine der andere umgedreht ist.
 func IsReverse(s1, s2 string) bool {
-
-	return s1 == Reverse(s2)
+	return s2 == Reverse(s1)
 
 }
 
@@ -17,6 +19,6 @@ func IsReverse(s1, s2 string) bool {
 // Ein Palindrom ist ein String, der vorwärts und rückwärts gelesen gleich ist.
 func IsPalindrome(s string) bool {
 
-	return IsReverse(s, s)
+	return s == Reverse(s)
 
 }

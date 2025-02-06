96cc8247fe6f1998d4e5445f58d6387e3a070969
Tomderandere <tompbickle@gmail.com>
2025-02-06 17:12:22 +0100

    duplicate chars

--- strings/duplicate_chars.go
+++ strings/duplicate_chars.go
@@ -3,6 +3,9 @@ package strings
 // Erwartet einen String s und liefert einen neuen String,
 // in dem jeder Buchstabe aus s zweimal hintereinander vorkommt.
 func DuplicateChars(s string) string {
-	// TODO
-	return ""
+	var result string
+	for n := 0; n < len(s); n++ {
+		result = string(result) + string(s[n]) + string(s[n])
+	}
+	return result
 }

62933b8345da8bd7b8c6577fbaacb14934c9f8c0
Tomderandere <tompbickle@gmail.com>
2025-02-09 09:15:58 +0100

    cmd line drawings

--- cmdline_drawing/rectangles/empty.go
+++ cmdline_drawing/rectangles/empty.go
@@ -1,10 +1,30 @@
 package rectangles
 
+import "fmt"
+
 // Erwartet zwei Seitenlängen `height` und `width`.
 // Zeichnet ein Rechteck mit diesen Seitenlängen auf der Konsole.
 // Der Rand des Rechtecks soll aus `#`-Zeichen bestehen, der Innenraum soll leer sein.
 func DrawEmptyRectangle(height, width int) {
-	// TODO
+	for i := 0; i < height; i++ {
+		if i == 0 || i == height-1 {
+			for j := 0; j < width; j++ {
+				fmt.Print("#")
+
+			}
+			fmt.Println()
+		}
+		if i != 0 && i != height-1 {
+			for j := 0; j < width; j++ {
+				if j == 0 || j == width-1 {
+					fmt.Print("#")
+				} else {
+					fmt.Print(" ")
+				}
+			}
+			fmt.Println()
+		}
+	}
 }
 
 // REMARKS
--- cmdline_drawing/rectangles/flexible.go
+++ cmdline_drawing/rectangles/flexible.go
@@ -1,10 +1,30 @@
 package rectangles
 
+import "fmt"
+
 // Erwartet zwei Seitenlängen `height` und `width`.
 // Zeichnet ein Rechteck mit diesen Seitenlängen auf der Konsole.
 // Die Zeichen für Rand und Füllung des Rechtecks werden als Parameter erwartet.
 func DrawRectangle(height, width int, inner, outer string) {
-	// TODO
+	for i := 0; i < height; i++ {
+		if i == 0 || i == height-1 {
+			for j := 0; j < width; j++ {
+				fmt.Print(outer)
+
+			}
+			fmt.Println()
+		}
+		if i != 0 && i != height-1 {
+			for j := 0; j < width; j++ {
+				if j == 0 || j == width-1 {
+					fmt.Print(outer)
+				} else {
+					fmt.Print(inner)
+				}
+			}
+			fmt.Println()
+		}
+	}
 }
 
 // REMARKS

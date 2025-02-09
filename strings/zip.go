13f17d4c3fbd4aca459f1157998c88d7658a8e20
Tomderandere <tompbickle@gmail.com>
2025-02-09 08:31:34 +0100

    zip zip

--- strings/zip.go
+++ strings/zip.go
@@ -4,6 +4,22 @@ package strings
 // Liefert einen String, der abwechselnd aus den Buchstaben des einen und des anderen
 // Strings besteht.
 func Zip(s1, s2 string) string {
-	// TODO
-	return ""
+	var lenmax int
+	if len(s1) >= len(s2) {
+		lenmax = len(s1)
+	} else {
+		lenmax = len(s2)
+	}
+
+	var result string
+
+	for i := 0; i < lenmax; i++ {
+		if i < len(s1) {
+			result = result + string(s1[i])
+		}
+		if i < len(s2) {
+			result = result + string(s2[i])
+		}
+	}
+	return result
 }
